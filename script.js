bc419184b9cbd8c66902da1c63fe3c80cd2a8de8
Shantanu Sutar <shantanusutar17@gmail.com>
2023-02-19 01:42:16 +0530

    Creating a notifications toast

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const buttons = document.querySelectorAll(".buttons .btn");
-
+const notifications = document.querySelector(".notif");
 const createToast = (id) => {
-  console.log(id);
+  const toast = document.createElement("section");
+  toast.className = `toast ${id}`;
+  toast.innerHTML = `<section class="column">
+                     <i class="fa-solid fa-circle-check"></i>
+                     <span>Success: This is a success toast</span>
+                     </section>
+                     <i class="fa-solid fa-xmark"></i>`;
+  notifications.appendChild(toast);
 };
 
 buttons.forEach((btn) => {
